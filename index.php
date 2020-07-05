cd3adc59db2241333538bb5574b9745593457708
akr007 <antalkrisztian0@gmail.com>
2020-07-05 15:27:22 +0200

    Converting HTML to PHP

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+    include('header.php');
+
+    include('./Template/_banner-area.php');
+    include('./Template/_top-sale.php');
+    include('./Template/_special-price.php');
+    include('./Template/_banner-adds.php');
+    include('./Template/_new-phones.php');
+    include('./Template/_blogs.php');
+
+    include('footer.php');
+?>
---
+++ product.php
@@ -0,0 +1,9 @@
+<?php
+    include ('header.php');
+
+    include ('./Template/_products.php');
+    include ('./Template/_top-sale.php');
+
+    include ('footer.php');
+?>
+
